489c35f44c2c699b240824774c403a5df67b6760
“maxwilsonpereira <maxwilsonpereira@gmail.com>
2021-04-22 13:56:15 +0200

    serverRUL to prod

--- src/serverURL.js
+++ src/serverURL.js
@@ -1,7 +1,7 @@
 // DEVELOPMENT:
-const serverURL = 'http://localhost:8080';
+// const serverURL = 'http://localhost:8080';
 
 // PRODUCTION:
-// const serverURL = "https://tax-app-backend.herokuapp.com";
+const serverURL = 'https://tax-app-backend.herokuapp.com';
 
 export default serverURL;
